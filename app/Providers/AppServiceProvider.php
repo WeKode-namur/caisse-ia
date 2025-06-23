60c0b2cf0a1283797dcf81899088733360531509
Damien <damien.jasmes@wekode.be>
2025-06-23 07:45:40 +0200

    Set default string length to 191 in AppServiceProvider to support older MySQL versions.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
 use App\Helpers\TransactionHelper;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
+
         // Rendre la fonction translateStatus disponible globalement dans les vues
         Blade::directive('translateStatus', function ($expression) {
             return "<?php echo App\\Helpers\\TransactionHelper::translateStatus($expression); ?>";
